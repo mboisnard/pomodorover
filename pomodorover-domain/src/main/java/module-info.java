b40bccb474bf64ce9b4f1ef903305d0ab545ce8c
Mathieu Boisnard <m.boisnard@free.fr>
2018-11-20 12:09:44 +0100

    Add Modules except for api

---
+++ pomodorover-domain/src/main/java/module-info.java
@@ -0,0 +1,4 @@
+module fr.lacombe.pomodorover.domain {
+    exports fr.lacombe.pomodorover.domain;
+    opens fr.lacombe.pomodorover.domain;
+}
---
+++ pomodorover-infra-mongo/src/main/java/module-info.java
@@ -0,0 +1,7 @@
+module fr.lacombe.pomodorover.infra.mongo {
+    requires mongo.java.driver;
+    requires fr.lacombe.pomodorover.domain;
+
+    opens fr.lacombe.pomodorover.infra.mongo;
+    exports fr.lacombe.pomodorover.infra.mongo;
+}
